dae798aaf0715bc92bdb67f99d4927546fe27f81
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2025-03-09 14:23:23 +0900

    Remove unusued migration

--- config/db/migrate/20250309120101_create_books.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-ROM::SQL.migration do
-  # Add your migration here.
-  #
-  # See https://guides.hanamirb.org/v2.2/database/migrations/ for details.
-  change do
-  end
-end
