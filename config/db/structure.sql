8e595dd89f41fec64b2ace79965927ef37298f4f
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2025-03-14 14:33:35 +0900

    Run `hanami db migrate`

--- config/db/structure.sql
+++ config/db/structure.sql
@@ -1,2 +1,8 @@
+CREATE TABLE `schema_migrations`(`filename` varchar(255) NOT NULL PRIMARY KEY);
+CREATE TABLE `books`(
+  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `title` text NOT NULL,
+  `author` text NOT NULL
+);
 INSERT INTO schema_migrations (filename) VALUES
 ('20250309111940_create_books.rb');
